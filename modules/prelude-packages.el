234c3b9dcf5dda648d8543264fda57fba7d35f59
Jupp Müller <jupp0r@gmail.com>
2012-01-27 13:09:55 +0100

    http fetch workaround

--- modules/prelude-packages.el
+++ modules/prelude-packages.el
@@ -38,6 +38,10 @@
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
+; this is a workaround for a bug in emacs' http fetching, see
+; http://lists.gnu.org/archive/html/bug-gnu-emacs/2011-12/msg00196.html
+(setq url-http-attempt-keepalives nil)
+
 (defvar prelude-packages
   '(auctex clojure-mode coffee-mode deft full-ack gist groovy-mode haml-mode
            haskell-mode inf-ruby markdown-mode paredit projectile
