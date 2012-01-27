3773691b00fdf179629a53b57a5ec80291467d46
Jupp Müller <jupp0r@gmail.com>
2012-01-27 09:54:00 +0100

    fix for packages

--- modules/prelude-packages.el
+++ modules/prelude-packages.el
@@ -35,9 +35,9 @@
 
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
+             '("tromey" . "http://tromey.com/elpa/"))
 (package-initialize)
 
 (defvar prelude-packages
